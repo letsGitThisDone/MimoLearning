147d29e56320623d4b9b3de1aa6793782484b253
Sergiy <biyata.sergiy@gmail.com>
2023-11-28 19:42:18 -0800

    Unit Conversion Project Finished.

---
+++ Interactive Web Pages/UnitConverter/script.js
@@ -0,0 +1,12 @@
+const inputValue = 10;
+const milesToKm = true;
+let result = 0;
+if (milesToKm) {
+  result = (inputValue * 1.60934);
+} else {
+  result = (inputValue / 1.60934);
+}
+const resultString = inputValue + " miles are " + result + " km";
+console.log(resultString);
+const resultElement = document.getElementById("resultElement");
+resultElement.innerHTML = resultString;
