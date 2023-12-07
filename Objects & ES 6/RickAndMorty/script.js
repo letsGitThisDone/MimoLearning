c7e5fc3385b315b9f356f0099b8c1fc281fe6be9
letsGitThisDone <biyata.sergiy@gmail.com>
2023-12-06 21:22:35 -0800

    Rick and Morty part1 (Boke after updated content)

---
+++ Objects & ES 6/RickAndMorty/script.js
@@ -0,0 +1,31 @@
+const data = {
+    characters: [{
+      "name": "Rick Sanchez",
+      "status": "Alive",
+      "species": "Human",
+      "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+    }, {
+      "name": "Morty Smith",
+      "status": "Alive",
+      "species": "Human",
+      "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    }, {
+      "name": "Summer Smith",
+      "status": "Alive",
+      "species": "Human",
+      "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+    }]
+  };
+  const charactersContainer = document.getElementById("characters-container");
+  data.characters.forEach(function(character) {
+    const characterCard = document.createElement(`div`);
+    characterCard.className = `card`;
+    charactersContainer.appendChild(characterCard);
+  
+    card.innerHTML = `
+    <img src="${character.image}" alt="${character.name}" class="character-image">
+    <p class="character-detail"><span class="attribute">Name:</span> ${character.name}</p>
+    <p class="character-detail"><span class="attribute">Status:</span> ${character.status}</p>
+    <p class="character-detail"><span class="attribute">Species:</span> ${character.species}</p>
+  `;
+  });
