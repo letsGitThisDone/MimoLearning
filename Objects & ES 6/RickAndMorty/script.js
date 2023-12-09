60932540a59d3c9a8bccef077c4d1a19e1b23118
letsGitThisDone <biyata.sergiy@gmail.com>
2023-12-08 19:16:52 -0800

    Rick and Morty Project Part2 (exposed to API)

--- Objects & ES 6/RickAndMorty/script.js
+++ Objects & ES 6/RickAndMorty/script.js
@@ -1,26 +1,11 @@
-const data = {
-  characters: [{
-    "name": "Rick Sanchez",
-    "status": "Alive",
-    "species": "Human",
-    "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-  }, {
-    "name": "Morty Smith",
-    "status": "Alive",
-    "species": "Human",
-    "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-  }, {
-    "name": "Summer Smith",
-    "status": "Alive",
-    "species": "Human",
-    "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-  }]
-};
+const data = {};
 const charactersContainer = document.getElementById("characters-container");
-data.characters.forEach(function(character) {
+fetch("https://rickandmortyapi.com/api/character").then(response => response
+  .json()).then(data => {data.results.forEach(function(character) {
   const newDiv = document.createElement("div");
   newDiv.classList.add("card");
   charactersContainer.appendChild(newDiv);
   newDiv.innerHTML =
     `<img src="${character.image}" alt="${character.name}" class="character-image"><p class="character-detail"><span class="attribute">Name:</span>${character.name}</p><p class="character-detail"><span class="attribute">Status:</span> ${character.status}</p><p class="character-detail"><span class="attribute">Species:</span> ${character.species}</p>`;
+  });
 });
