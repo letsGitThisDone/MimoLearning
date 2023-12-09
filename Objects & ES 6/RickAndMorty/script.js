81adc10fcf58a91db9fbb4abd85c1a634a423111
letsGitThisDone <biyata.sergiy@gmail.com>
2023-12-08 18:07:38 -0800

    Rick and Morty Project Part 1 (fixed)

--- Objects & ES 6/RickAndMorty/script.js
+++ Objects & ES 6/RickAndMorty/script.js
@@ -1,31 +1,26 @@
 const data = {
-    characters: [{
-      "name": "Rick Sanchez",
-      "status": "Alive",
-      "species": "Human",
-      "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-    }, {
-      "name": "Morty Smith",
-      "status": "Alive",
-      "species": "Human",
-      "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-    }, {
-      "name": "Summer Smith",
-      "status": "Alive",
-      "species": "Human",
-      "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-    }]
-  };
-  const charactersContainer = document.getElementById("characters-container");
-  data.characters.forEach(function(character) {
-    const characterCard = document.createElement(`div`);
-    characterCard.className = `card`;
-    charactersContainer.appendChild(characterCard);
-  
-    card.innerHTML = `
-    <img src="${character.image}" alt="${character.name}" class="character-image">
-    <p class="character-detail"><span class="attribute">Name:</span> ${character.name}</p>
-    <p class="character-detail"><span class="attribute">Status:</span> ${character.status}</p>
-    <p class="character-detail"><span class="attribute">Species:</span> ${character.species}</p>
-  `;
-  });
+  characters: [{
+    "name": "Rick Sanchez",
+    "status": "Alive",
+    "species": "Human",
+    "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  }, {
+    "name": "Morty Smith",
+    "status": "Alive",
+    "species": "Human",
+    "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+  }, {
+    "name": "Summer Smith",
+    "status": "Alive",
+    "species": "Human",
+    "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+  }]
+};
+const charactersContainer = document.getElementById("characters-container");
+data.characters.forEach(function(character) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("card");
+  charactersContainer.appendChild(newDiv);
+  newDiv.innerHTML =
+    `<img src="${character.image}" alt="${character.name}" class="character-image"><p class="character-detail"><span class="attribute">Name:</span>${character.name}</p><p class="character-detail"><span class="attribute">Status:</span> ${character.status}</p><p class="character-detail"><span class="attribute">Species:</span> ${character.species}</p>`;
+});
