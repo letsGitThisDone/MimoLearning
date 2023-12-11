f05163b531f00fc2b44905ec6d15c54b2d270e16
letsGitThisDone <biyata.sergiy@gmail.com>
2023-12-10 20:37:42 -0800

    Comedian Bot Part2. hide button =  0 sense.

--- Synchrony & Asynchrony in JS/Comedian Bot/script.js
+++ Synchrony & Asynchrony in JS/Comedian Bot/script.js
@@ -1,4 +1,8 @@
-const joke = ["What did 0 say to 8?", "Nice belt."];
+let jokeIndex = 0;
+const jokes = [
+  ["What did 0 say to 8?", "Nice belt."],
+  ["If the internet is a boat, where would they park it?", "Google doc."]
+];
 const chatContent = document.querySelector(".chat-content");
 
 function appendBotMessage(messageText) {
@@ -15,12 +19,41 @@ function appendBotMessage(messageText) {
 }
 appendBotMessage(
   "Hello! I am Comedian Bot, her to make you laugh. Let me think of a joke.");
-setTimeout(function() {
-  appendBotMessage("Ok, got one.");
-}, 1000);
-setTimeout(function() {
-  appendBotMessage(joke[0]);
-}, 1500);
-setTimeout(function() {
-  appendBotMessage(joke[1]);
-}, 2000);
+
+function requestJoke() {
+  appendUserMessage();
+  if (jokeIndex >= jokes.length) {
+    appendBotMessage("Sorry, I'm out of jokes for now!");
+    return;
+  }
+  /*Dont understand why i was instructed to hide joke button after all 
+  the work of making the bot capable of saying multiple jokes. 
+  */
+  hideButton.style.display = "none";
+  setTimeout(function() {
+    appendBotMessage("Ok, got one.");
+  }, 1000);
+  setTimeout(function() {
+    appendBotMessage(jokes[jokeIndex][0]);
+  }, 1500);
+  setTimeout(function() {
+    appendBotMessage(jokes[jokeIndex][1]);
+    jokeIndex++;
+    hideButton.style.display = inline - block;
+  }, 2000);
+}
+
+function appendUserMessage(messageText) {
+  const messageDiv = document.createElement("div");
+  messageDiv.className = "message user-message";
+  chatContent.appendChild(messageDiv);
+  const avatar = document.createElement("div");
+  avatar.className = "fas fa-smile message-avatar";
+  messageDiv.appendChild(avatar);
+  const contentDiv = document.createElement("div");
+  contentDiv.className = "message-content";
+  messageText = "Tell me a joke";
+  contentDiv.textContent = messageText;
+  messageDiv.appendChild(contentDiv);
+}
+const hideButton = document.getElementById("joke-button");
