bced136d8b3567433648b2c93eec5fb44b68a9f0
letsGitThisDone <biyata.sergiy@gmail.com>
2023-12-20 14:09:08 -0800

    Pixel Paws Part2_Fully Functioning

--- Javascript Classes/script.js
+++ Javascript Classes/script.js
@@ -8,11 +8,11 @@ class Pet {
     }
     feed(amount) {
       this.fedLevel = Math.min((this.fedLevel + amount), 100);
-      fedElement = this.fedLevel;
+      this.fedElement.innerText = this.fedLevel;
     }
     decreaseFedLevel() {
       this.fedLevel = Math.max((this.fedLevel - 10), 0);
-      this.fedElement.innerHTML = this.fedLevel;
+      this.fedElement.innerText = this.fedLevel;
       if (this.fedLevel === 0) {
         this.messageElement.innerText = "Pet ran away";
         this.displayElement.style.opacity = "0.5";
@@ -20,10 +20,30 @@ class Pet {
     }
   }
   const pet1 = new Pet(1);
+  const pet2 = new Pet(2);
+  let selectedPet = null;
+  const pet1Element = document.getElementById("pet1");
+  const pet2Element = document.getElementById("pet2");
   
   function feedPet(amount) {
-    pet1.feed(amount);
+    if (selectedPet === 1) {
+      pet1.feed(amount);
+    } else if (selectedPet === 2) {
+      pet2.feed(amount);
+    }
   }
   setInterval(() => {
     pet1.decreaseFedLevel();
+    pet2.decreaseFedLevel();
   }, 1000);
+  
+  function selectPet(id) {
+    selectedPet = id;
+    if (selectedPet === 1) {
+      pet1Element.classList.add("selected");
+      pet2Element.classList.remove("selected");
+    } else if (selectedPet === 2) {
+      pet2Element.classList.add("selected");
+      pet1Element.classList.remove("selected");
+    }
+  }
